42743c263af343ab71fcdf5edcdb50449dc2703d
Sway <sway@allconnected.co>
2017-01-09 16:19:51 +0800

    Update: 修订常量 value

--- src/main/java/co/allconnected/ad/utils/ADConstants.java
+++ src/main/java/co/allconnected/ad/utils/ADConstants.java
@@ -16,28 +16,28 @@ public class ADConstants {
         /**
          * 广告位-返回app
          */
-        public static final String RETURN_APP = "1";
+        public static final String RETURN_APP = "return_app";
 
         /**
          * 广告位-退出app
          */
-        public static final String EXIT_APP = "2";
+        public static final String EXIT_APP = "exit_app";
         /**
          * 广告位-VIP
          */
-        public static final String VIP = "3";
+        public static final String VIP = "vip";
         /**
          * 广告位-连接vpn成功
          */
-        public static final String CONNECT_VPN = "4";
+        public static final String CONNECT_VPN = "connected_vpn";
         /**
          * 广告位-断开vpn连接
          */
-        public static final String DISCONNECT_VPN = "5";
+        public static final String DISCONNECT_VPN = "disconnect_vpn";
         /**
          * 广告位-protector
          */
-        public static final String PROTECTOR = "6";
+        public static final String PROTECTOR = "protector";
     }
 
     /**
@@ -48,12 +48,12 @@ public class ADConstants {
         /**
          * 广告类型-全屏
          */
-        public static final String AD_TYPE_FULLSCREEN = "1";
+        public static final String AD_TYPE_FULLSCREEN = "fullscreen";
 
         /**
          * 广告类型-原生
          */
-        public static final String AD_TYPE_NATIVE = "2";
+        public static final String AD_TYPE_NATIVE = "native";
     }
 
     /**
@@ -64,12 +64,12 @@ public class ADConstants {
         /**
          * 广告平台-facebook
          */
-        public static final String AD_PLATFORM_FACEBOOK = "1";
+        public static final String AD_PLATFORM_FACEBOOK = "facebook";
 
         /**
          * 广告平台-du
          */
-        public static final String AD_PLATFORM_DU = "2";
+        public static final String AD_PLATFORM_DU = "baidu";
     }
 
     /**
