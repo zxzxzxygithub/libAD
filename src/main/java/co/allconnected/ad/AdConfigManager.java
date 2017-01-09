52b3c965d50ded808ced8ede11ea796eae6d2c14
zheng <michael@acnet.co>
2017-01-09 14:06:25 +0800

    update:一些常量的定义

--- src/main/java/co/allconnected/ad/AdConfigManager.java
+++ src/main/java/co/allconnected/ad/AdConfigManager.java
@@ -1,25 +1,35 @@
 package co.allconnected.ad;
 
+import co.allconnected.ad.bean.AdConfigBean;
+
 /**
  * 广告配置管理类
  * @author michael
  * @time 17/1/9 上午10:32
  */
-public interface AdConfigManager<T> {
+public interface AdConfigManager{
 
     /**
      * 获取某个广告位的配置
+     * @param adPositionId  广告位id
      */
-    T getAdConfigBean(T t);
+    AdConfigBean getAdConfigBean(String adPositionId);
+
 
     /**
      * 保存所有广告位的配置
      */
     void saveAdConfigBeanes();
 
+
     /**
      * 获取在线参数
      */
-    void getAdOnlineConfig();
+    String getAdOnlineConfig();
+
+
+
+
+
 
 }
--- src/main/java/co/allconnected/ad/utils/ADConstants.java
+++ src/main/java/co/allconnected/ad/utils/ADConstants.java
@@ -9,31 +9,92 @@ package co.allconnected.ad.utils;
 public class ADConstants {
 
     /**
-     * 广告位-返回app
+     * 广告位
+     *
+     * @author michael
+     * @time 17/1/9 下午2:03
      */
-   public static final String AD_POSITION_RETURNAPP="1";
+    public static class AdPosition {
+
+        /**
+         * 广告位-返回app
+         */
+        public static final String RETURNAPP = "1";
+
+        /**
+         * 广告位-退出app
+         */
+        public static final String EXITAPP = "2";
+        /**
+         * 广告位-VIP
+         */
+        public static final String VIP = "3";
+        /**
+         * 广告位-连接vpn成功
+         */
+        public static final String CONNECT_VPN = "4";
+        /**
+         * 广告位-断开vpn连接
+         */
+        public static final String DISCONNECT_VPN = "5";
+        /**
+         * 广告位-protector
+         */
+        public static final String PROTECTOR = "6";
+
+
+    }
 
     /**
-     * 广告位-退出app
+     * 广告类型
+     *
+     * @author michael
+     * @time 17/1/9 下午2:04
      */
-    public static final String AD_POSITION_EXITAPP="2";
-    /**
-     * 广告位-VIP
-     */
-    public static final String AD_POSITION_VIP="3";
-    /**
-     * 广告位-连接vpn成功
-     */
-    public static final String AD_POSITION_CONNECT_VPN="4";
-    /**
-     * 广告位-断开vpn连接
-     */
-    public static final String AD_POSITION_DISCONNECT_VPN="5";
-    /**
-     * 广告位-protector
-     */
-    public static final String AD_POSITION_PROTECTOR="6";
+    public static class AdType {
 
+        /**
+         * 广告类型-全屏
+         */
+        public static final String AD_TYPE_FULLSCREEN = "1";
+
+        /**
+         * 广告类型-原生
+         */
+        public static final String AD_TYPE_NATIVE = "2";
+
+    }
+
+    /**
+     * 广告平台
+     *
+     * @author michael
+     * @time 17/1/9 下午2:04
+     */
+    public static class AdPlatform {
+
+        /**
+         * 广告平台-facebook
+         */
+        public static final String AD_PLATFORM_FACEBOOK = "1";
+
+        /**
+         * 广告平台-du
+         */
+        public static final String AD_PLATFORM_DU = "2";
+
+    }
+
+    /**
+     * 广告统计
+     *
+     * @author michael
+     * @time 17/1/9 下午2:04
+     */
+    public static class AdStats {
+
+
+    }
 
 
 }
